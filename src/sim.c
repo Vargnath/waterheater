c70ca98d932ce179bbcd4d6b4c4598bc3a50bf8a
Vargnath <resh@mailbox.org>
2016-11-14 00:07:05 +0100

    Added copyright claimer

--- src/sim.c
+++ src/sim.c
@@ -1,8 +1,23 @@
+/* WaterHeater - Simple water heating simulation written in C
+ * Copyright (C) 2015 Sebastian Kaiser
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
 #include <errno.h>
 #include <math.h>
 #include <stdlib.h>
 #include <pthread.h>
-//#include <time.h>
 #include <unistd.h>
 #include "sim.h"
 
